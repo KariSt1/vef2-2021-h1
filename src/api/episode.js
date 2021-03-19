3ff07da17a657442af22d6f6a40f23fb78d14c84
Anna María Steingrímsdóttir <annamaria@Annas-MacBook-Air.local>
2021-03-19 20:34:41 +0000

    Bætti við smá í episode.js og genres.js

--- src/api/episode.js
+++ src/api/episode.js
@@ -1,8 +1,15 @@
 import { query, pagedQuery } from '../utils/db.js';
-import addPageMetadata from '../utils/addPageMetadata.js';
 
 export async function listEpisode(req, res) {
-
+    const { id } = req.params;
+  
+    const episode = await findById(id);
+  
+    if (!episode) {
+      return res.status(404).json({ error: 'Episode not found' });
+    }
+  
+    return res.json(episode);
 }
 
 export async function newEpisode(req, res) {
