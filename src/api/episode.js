134cfc1d009450344096319bd86e6de9f8eba4c1
Anna María Steingrímsdóttir <annamaria@Annas-MacBook-Air.local>
2021-03-19 20:30:28 +0000

    Setja inn js skrár sem eru eftir

---
+++ src/api/episode.js
@@ -0,0 +1,14 @@
+import { query, pagedQuery } from '../utils/db.js';
+import addPageMetadata from '../utils/addPageMetadata.js';
+
+export async function listEpisode(req, res) {
+
+}
+
+export async function newEpisode(req, res) {
+
+}
+
+export async function deleteEpisode(req, res) {
+
+}
---
+++ src/api/genres.js
@@ -0,0 +1,10 @@
+import { query, pagedQuery } from '../utils/db.js';
+import addPageMetadata from '../utils/addPageMetadata.js';
+
+export async function listGenres(req, res) {
+
+}
+
+export async function newGenre(req, res) {
+
+}
