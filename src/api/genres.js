7bf247de12273e1527f05028d49f450dc1b2b310
Kari <kari_steinn@outlook.com>
2021-03-22 03:46:46 +0000

    lint

--- src/api/genres.js
+++ src/api/genres.js
@@ -1,10 +1,9 @@
 import xss from 'xss';
 import { query, pagedQuery } from '../utils/db.js';
 import { addPageMetadata } from '../utils/addPageMetadata.js';
-import { isNotEmptyString, lengthValidationError } from '../utils/validation.js';
+import { isNotEmptyString, lengthValidationError, isEmpty } from '../utils/validation.js';
 
-async function validateGenre(name, patching = false,
-  id = null,) {
+async function validateGenre(name, patching = false) {
   const validation = [];
   if (!patching || name || isEmpty(name)) {
     if (!isNotEmptyString(name, { min: 1, max: 128 })) {
@@ -15,45 +14,44 @@ async function validateGenre(name, patching = false,
       });
     }
   }
-  
+
   return validation;
 }
 
 export async function listGenres(req, res) {
-    const { offset = 0, limit = 10 } = req.query;
+  const { offset = 0, limit = 10 } = req.query;
 
-    const genres = await pagedQuery(
-        `SELECT
-          name
-        FROM
-          genres `,
-        [],
-        { offset, limit },
-      );
+  const genres = await pagedQuery(
+    `SELECT
+      name
+    FROM
+      genres `,
+    [],
+    { offset, limit },
+  );
 
-    const genresWithPage = addPageMetadata(
-        genres,
-        req.path,
-        { offset, limit, length: genres.items.length },
-      );
-    
-      return res.json(genresWithPage);
+  const genresWithPage = addPageMetadata(
+    genres,
+    req.path,
+    { offset, limit, length: genres.items.length },
+  );
+
+  return res.json(genresWithPage);
 }
 
 export async function newGenre(req, res) {
   const { name } = req.body;
-  
+
   const validations = await validateGenre(name);
-  
+
   if (validations.length > 0) {
     return res.status(400).json({
       errors: validations,
     });
   }
-  
+
   const q = 'INSERT INTO genres (name) VALUES ($1) RETURNING name';
   const result = await query(q, [xss(name)]);
-  
-  return res.status(201).json(result.rows[0]);
 
+  return res.status(201).json(result.rows[0]);
 }
